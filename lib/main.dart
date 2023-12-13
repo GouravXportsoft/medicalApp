da0f3d0b48fea592d4c32dd114caf8a0c15fa546
Air123Akash2 <akash.xportsoft@gmail.com>
2023-12-13 12:16:08 +0530

    image uploading screen done

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
       home: const LabScreen(),
     );
   }
