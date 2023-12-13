f0b12f756d2a35c20a61f51b1871ed9f3f43dfaf
Air123Akash@ <akash.xportsoft@gmail.com>
2023-12-13 11:08:23 +0530

    first push

---
+++ lib/labscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LabScreen extends StatefulWidget {
+  const LabScreen({super.key});
+
+  @override
+  State<LabScreen> createState() => _LabScreenState();
+}
+
+class _LabScreenState extends State<LabScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
