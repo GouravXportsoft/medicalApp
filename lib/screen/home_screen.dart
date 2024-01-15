967bb1d9e6732ac277f71aa61b4ebf08d5e2b028
gouravlohat <gauravlohat2468@gmail.com>
2024-01-15 10:11:17 +0530

    ui update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -32,100 +32,81 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           const CustomeAppBar(),
-          Expanded(
-            child: SingleChildScrollView(
+          Stack(children: [
+            // Image.asset(
+            //   bgImg,
+            //   color: const Color(0xff91C9C4),
+            // ),
+            Expanded(
               child: Stack(children: [
-                // Image.asset(
-                //   bgImg,
-                //   color: const Color(0xff91C9C4),
-                // ),
-                Expanded(
-                  child: Stack(children: [
-                    Positioned(
-                      bottom: -60,
-                      child: Image.asset(
-                        bgImg,
-                        color: bgImageColor,
-                      ),
-                    ),
-                    SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 27),
-                        child: Column(
-                          children: [
-                            Image.asset(bannerImg),
-                            const SizedBox(
-                              height: 30,
-                            ),
-                            GridView.builder(
-                                physics: const NeverScrollableScrollPhysics(),
-                                itemCount: gridImages.length,
-                                shrinkWrap: true,
-                                gridDelegate:
-                                    const SliverGridDelegateWithFixedCrossAxisCount(
-                                        childAspectRatio: 14 / 9,
-                                        crossAxisCount: 2,
-                                        mainAxisSpacing: 10,
-                                        crossAxisSpacing: 10),
-                                itemBuilder: ((context, index) {
-                                  return InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  LabUpload()));
-                                    },
-                                    child: Container(
-                                      height: 60,
-                                      width: 60,
-                                      decoration: BoxDecoration(
-                                          gradient: LinearGradient(
-                                              colors: [
-                                                Color(0xff55BE00),
-                                                Color(0xff3171DD)
-                                              ],
-                                              end: Alignment.bottomRight,
-                                              begin: Alignment.topLeft),
-                                          borderRadius:
-                                              BorderRadius.circular(8)),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Image.asset(
-                                            gridImages[index],
-                                            scale: 2,
-                                          ),
-                                          const SizedBox(
-                                            height: 7,
-                                          ),
-                                          Text(
-                                            gridImagesText[index],
-                                            style: const TextStyle(
-                                                fontWeight: FontWeight.w700),
-                                          )
-                                        ],
+                Positioned(
+                  bottom: -60,
+                  child: Image.asset(
+                    bgImg,
+                    color: bgImageColor,
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 27),
+                    child: Column(
+                      children: [
+                        Image.asset(bannerImg),
+                        GridView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: gridImages.length,
+                            shrinkWrap: true,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    childAspectRatio: 14 / 9,
+                                    crossAxisCount: 2,
+                                    mainAxisSpacing: 10,
+                                    crossAxisSpacing: 10),
+                            itemBuilder: ((context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => LabUpload()));
+                                },
+                                child: Container(
+                                  height: 60,
+                                  width: 60,
+                                  decoration: BoxDecoration(
+                                      color: lightGreenColor,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Image.asset(
+                                        gridImages[index],
+                                        scale: 2,
                                       ),
-                                    ),
-                                  );
-                                }))
-                          ],
-                        ),
-                      ),
+                                      const SizedBox(
+                                        height: 7,
+                                      ),
+                                      Text(
+                                        gridImagesText[index],
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w700),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }))
+                      ],
                     ),
-                  ]),
+                  ),
                 ),
               ]),
             ),
-          ),
+          ]),
           const SizedBox(
             height: 20,
           ),
-          const BottomContainer(),
-          const SizedBox(
-            height: 20,
-          ),
+          const BottomContainer()
         ],
       ),
     );
